2227e12e110edd49f383518f2aa76d4ce5e00a6b
DonFranklie <donfranklie@gmail.com>
2024-02-23 19:03:41 +0300

    toast provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
     <ClerkProvider>
         <html lang="en">
           <body className={inter.className}>
+          <ToastProvider/>
             {children}
-            <ToastProvider/>
           </body>
        </html>
     </ClerkProvider>
