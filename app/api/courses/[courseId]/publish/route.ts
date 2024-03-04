cb32369a0a6ff7f5bb3421698727ab40334b2ac5
DonFranklie <donfranklie@gmail.com>
2024-03-04 22:30:05 +0300

    publish paper route

---
+++ app/api/courses/[courseId]/publish/route.ts
@@ -0,0 +1,48 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { courseId: string } }
+) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const course = await db.course.findUnique({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+    });
+
+    if (!course) {
+      return new NextResponse("Not found", { status: 404 });
+    }
+
+
+    if (!course.title || !course.description || !course.course || !course.department || !course.college || !course.unitCode  || !course.school) {
+      return new NextResponse("Missing required fields", { status: 401 });
+    }
+
+    const publishedCourse = await db.course.update({
+      where: {
+        id: params.courseId,
+        userId,
+      },
+      data: {
+        isPublished: true,
+      }
+    });
+
+    return NextResponse.json(publishedCourse);
+  } catch (error) {
+    console.log("[COURSE_ID_PUBLISH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  } 
+}
