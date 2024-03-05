6cf24f090d4ca7520ed69166f1931a6d433a259d
DonFranklie <donfranklie@gmail.com>
2024-03-05 16:01:51 +0300

    past papers page

--- app/(dashboard)/(routes)/past-papers/page.tsx
+++ app/(dashboard)/(routes)/past-papers/page.tsx
@@ -1,8 +1,41 @@
-const PastPapers = () => {
+import { getCourses } from "@/actions/get-papers";
+import { CourseList } from "@/components/courses-list";
+import { SearchInput } from "@/components/search-input";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface SearchPageProps {
+  searchParams: {
+    title: string;
+  }
+}
+
+const PastPapers = async ({
+  searchParams
+}: SearchPageProps) => {
+  const { userId } = auth();
+  if(!userId) {
+    return redirect("/")
+  }
+
+
+  const courses = await getCourses({
+    userId,
+    ...searchParams
+  })
+
   return (
-    <div>
-      This is a PastPapers page
-    </div>
+<>
+<div className="px-6 pt-6 md:hidden md:mb-0 block">
+  <SearchInput/>
+</div>
+
+<div className="p-6">
+    <CourseList items={courses}/>
+</div>
+</>
+
+  
   );
 
 }
