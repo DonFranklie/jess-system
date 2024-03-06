09e7d1f122068bcd6818dae16863dae3059954e6
DonFranklie <donfranklie@gmail.com>
2024-03-06 16:05:41 +0300

    button and link from next/nav

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -3,6 +3,8 @@
 import { UserButton } from "@clerk/nextjs";
 import { User } from "lucide-react";
 import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   const {user} = useUser();
@@ -13,7 +15,7 @@ export default function Home() {
         afterSignOutUrl="/"
       /> */}
 
-      <h1 className="mb-8 text-2xl sm:text-xl font-semibold text-slate-600">
+      <h1 className="mb-8 text-2xl sm:text-2xl font-semibold text-slate-600">
         Welcome to JIESS, {user?.firstName} 
       </h1>
 
@@ -27,17 +29,17 @@ export default function Home() {
 
           <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                 <a href="#">
-                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Past Papers</h5>
+                    <h5 className="mb-2 text-2xl sm:text-xl font-bold tracking-tight text-gray-900 dark:text-white">Past Papers</h5>
                 </a>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
                     Access a vast repository of past examination papers to enhance your study and exam preparation in your academic journey at JKUAT.
                 </p>
-                <a href="/past-papers" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    Access Past Papers
-                    <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                <Link href="/past-papers">
+                    <Button className="sm:text-sm">Access Past Papers <svg className="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                    </svg>
-                </a>
+                    </svg></Button>
+                    
+                </Link>
             </div>
 
             <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
