0dc4c0f75fd2c5c30bc41f85ab3c363545910337
DonFranklie <donfranklie@gmail.com>
2024-03-11 18:57:51 +0300

    resource add UI

---
+++ app/(dashboard)/(routes)/teacher/add/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import toast from "react-hot-toast";
+
+
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormLabel,
+  FormMessage,
+  FormItem,
+} from "@/components/ui/form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+});
+
+
+
+const addResource = ( ) => {
+  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: ""
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+   console.log(values);
+     try {
+      const response = await axios.post("/api/resources", values);
+      router.push(`/teacher/resources/${response.data.id}`);
+      toast.success("Resource instance created");
+    } catch {
+      toast.error("Something went wrong");
+    }
+  }
+ 
+
+  return (
+    <div className="max-w-5xl mx-auto flex md:items-center md:justify-center h-full p-6">
+     <div>
+      <h1 className="text-2xl">
+        Resource name
+      </h1>
+
+      <p className="text-sm text-slate-600">
+        What is the name of this Resource? You can change it later
+      </p>
+
+      <Form {...form}>
+        <form  
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8 mt-8">
+
+          <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                   Resource title
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'How to Write an Attachment Report'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    Resource Description?
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Link href="/teacher/resources">
+                <Button
+                  type="button"
+                  variant="ghost"
+                >
+                  Cancel
+                </Button>
+              </Link>
+              <Button
+                type="submit"
+                disabled={!isValid || isSubmitting}
+              >
+                Continue
+              </Button>
+            </div>
+          </form>
+
+      </Form>
+
+     </div>
+    </div>
+  )
+}
+
+export default addResource;
