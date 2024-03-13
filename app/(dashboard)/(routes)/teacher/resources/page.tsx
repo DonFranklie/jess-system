dfaf985d673d570d9b72d2caccf45ae95ef4a038
DonFranklie <donfranklie@gmail.com>
2024-03-13 17:55:00 +0300

    resource page -  admin mode

--- app/(dashboard)/(routes)/teacher/resources/page.tsx
+++ app/(dashboard)/(routes)/teacher/resources/page.tsx
@@ -1,14 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
+import { redirect } from "next/navigation";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
-const ResourcesPage = () => {
+const ResourcesPage = async () => {
+  const { userId } = auth();
+
+if (!userId) {
+  return redirect("/");
+}
+
+const courses = await db.resource.findMany({
+  where: {
+    userId,
+  },
+  orderBy: {
+    createdAt: "desc"
+  }
+})
+
   return ( 
     <div className="p-6">
-     <Link href="/teacher/add">
-     <Button>
-      New Resource
-     </Button>
-     </Link>
+     <DataTable columns={columns} data={courses} />
     </div>
    );
 }
