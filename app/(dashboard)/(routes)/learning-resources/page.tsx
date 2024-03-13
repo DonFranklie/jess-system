0b5d71b9e75f2cd9f2caaed5dc80e5edbcd8157a
DonFranklie <donfranklie@gmail.com>
2024-03-13 17:54:41 +0300

    search params for resources

--- app/(dashboard)/(routes)/learning-resources/page.tsx
+++ app/(dashboard)/(routes)/learning-resources/page.tsx
@@ -1,8 +1,39 @@
-const LearningResourcesPage = () => {
+import { getResources } from "@/actions/get-resources";
+import { ResourceSearchInput } from "@/components/resource-search-input";
+import { ResourcesList } from "@/components/resources-list";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface ResourcePageProps {
+  searchParams: {
+    title: string;
+  }
+}
+
+const LearningResourcesPage = async ({
+  searchParams
+}: ResourcePageProps) => {
+  const { userId } = auth();
+  if(!userId) {
+    return redirect("/")
+  }
+
+
+  const resources = await getResources({
+    userId,
+    ...searchParams
+  })
+
   return (
-    <div>
-      This is a LearningResourcesPage
+    <>
+    <div className="px-6 pt-6 md:hidden md:mb-0 block">
+      <ResourceSearchInput/>
     </div>
+    
+    <div className="p-6">
+        <ResourcesList items={resources}/>
+    </div>
+    </>
   );
 
 }
