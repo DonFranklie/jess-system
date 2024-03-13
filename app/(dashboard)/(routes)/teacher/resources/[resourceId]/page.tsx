d04e47d850f011ca481f26194cae3b71057cb9e0
DonFranklie <donfranklie@gmail.com>
2024-03-13 17:55:13 +0300

    resource setup page

--- app/(dashboard)/(routes)/teacher/resources/[resourceId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/resources/[resourceId]/page.tsx
@@ -1,11 +1,15 @@
 import { IconBadge } from "@/components/icon-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { LayoutDashboard } from "lucide-react";
+import { File, LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { TitleForm } from "./_components/title-form";
 import { DescriptionForm } from "./_components/description-form";
 import { ImageForm } from "./_components/image-form";
+import { AuthorForm } from "./_components/author-form";
+import { DocumentForm } from "./_components/document-form";
+import { Banner } from "@/components/banner";
+import { Actions } from "./_components/actions";
 
 
 const ResourceIdPage = async ({
@@ -45,8 +49,6 @@ const ResourceIdPage = async ({
     resource.title,
     resource.author,
     resource.description,
-    resource.school,
-    resource.imageUrl,
     resource.documents,
   ];
 
@@ -59,6 +61,14 @@ const ResourceIdPage = async ({
   
 
   return ( 
+    <>
+    {!resource.isPublished && (
+      <Banner
+        label="This resource is unpublished. It will not be visible to students"
+      />
+    )}
+
+
     <div className="p-6">
      <div className="flex items-center justify-between">
         <div className="flex flex-col gap-y-2">
@@ -70,6 +80,13 @@ const ResourceIdPage = async ({
             </span>
 
         </div>
+        {/* Add Actions */}
+        <Actions
+          disabled={!isComplete}
+          resourceId={params.resourceId}
+          isPublished={resource.isPublished}
+        
+        />
      </div>
       
      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-12">
@@ -91,14 +108,34 @@ const ResourceIdPage = async ({
             resourceId={resource.id}
             />
 
-            <ImageForm
+            <AuthorForm
             initialData={resource}
             resourceId={resource.id}
             />
 
+           
+
      </div>
+     {/* <ImageForm
+            initialData={resource}
+            resourceId={resource.id}
+            /> */}
+        <div>
+           <div className="flex items-center gap-x-2">
+            <IconBadge icon={File}/>
+            <h2 className="text-lg">Resource</h2>
+           </div>
+           
+           
+           <DocumentForm
+            initialData={resource}
+            resourceId={resource.id}
+            />
+        </div>
+          
     </div>
     </div>
+    </>
    );
 }
  
