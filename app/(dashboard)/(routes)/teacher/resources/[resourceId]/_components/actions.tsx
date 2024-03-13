ff315aade0c1e182f2094f39498e46effee2041d
DonFranklie <donfranklie@gmail.com>
2024-03-13 17:53:22 +0300

    publish and unpublish functions for a resource

---
+++ app/(dashboard)/(routes)/teacher/resources/[resourceId]/_components/actions.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+
+interface ActionsProps {
+  disabled: boolean;
+  resourceId: string;
+  isPublished: boolean;
+};
+
+export const Actions = ({
+  disabled,
+  resourceId,
+  isPublished
+}: ActionsProps) => {
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(`/api/resources/${resourceId}/unpublish`);
+        toast.success("Resource unpublished");
+      } else {
+        await axios.patch(`/api/resources/${resourceId}/publish`);
+        toast.success("Resource published");
+        confetti.onOpen();
+      }
+
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/resources/${resourceId}`);
+
+      toast.success("Resource deleted");
+      router.refresh();
+      router.push(`/teacher/resources`);
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onClick}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm"
+      >
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  )
+}
