4773f9b1e4d73222b2165cf7da6ef0936313a742
DonFranklie <donfranklie@gmail.com>
2024-03-16 16:16:26 +0300

    added text to logo component

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Logo = () => {
   return (
-    <Image
-    height={130}
-    width={130}
+  
+    <div className="flex items-center gap-x-3 justify-start">
+      <Image
+    height={30}
+    width={30}
     alt="logo"
     src="/logo.svg" 
-    />
+    /> 
+    <Link href="/">
+    <span className="mt-2 text-2xl text-slate-600 font-bold" >JIESS</span>
+    </Link>
+    </div>
+
   )
 }
