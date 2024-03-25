302b4f3e17901b09e013e69dc85bb6e6f3b95b6a
DonFranklie <donfranklie@gmail.com>
2024-03-25 08:08:30 +0300

    Video forms

---
+++ app/(dashboard)/(routes)/teacher/films/[filmId]/_components/author-form.tsx
@@ -0,0 +1,130 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Pencil } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Video } from "@prisma/client";
+import { cn } from "@/lib/utils";
+
+interface AuthorFormProps {
+  initialData: Video;
+  filmId: string;
+};
+
+const formSchema = z.object({
+  author: z.string().min(1, {
+    message: "Author Name(s) is required",
+  }),
+});
+
+
+
+export const AuthorForm = ({
+  initialData,
+  filmId,
+}: AuthorFormProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const toggleEdit = () => setIsEditing((current) => !current);
+
+  const router = useRouter();
+
+
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      author: initialData?.author || ""
+    },
+  });
+
+  const { isSubmitting, isValid } = form.formState;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => { 
+    try {
+      await axios.patch(`/api/films/${filmId}`, values);
+      toast.success("Author name(s) updated");
+      toggleEdit();
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    }
+  }
+
+  return(
+    <div className="mt-6 border bg-slate-100 rounded-md p-4">
+      <div className="font-medium flex items-center justify-between">
+        Author Name(s)
+        <Button onClick={toggleEdit} variant="ghost">
+        {isEditing ? (
+            <>Cancel</>
+          ) : (
+            <>
+              <Pencil className="h-4 w-4 mr-2" />
+              Edit Author
+            </>
+          )}
+          
+        </Button>
+      </div>
+
+      {!isEditing && (
+        <p className={cn(
+          "text-sm mt-2",
+          !initialData.author && "text-slate-500 italic"
+        )}>
+          {initialData.author || "No Author name(s)"}
+        </p>
+      )}
+
+{isEditing && (
+        <Form {...form}>
+          <form
+            onSubmit={form.handleSubmit(onSubmit)}
+            className="space-y-4 mt-4"
+          >
+            <FormField
+              control={form.control}
+              name="author"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      disabled={isSubmitting}
+                      placeholder="e.g. 'Name of Author(s)'"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div className="flex items-center gap-x-2">
+              <Button
+                disabled={!isValid || isSubmitting}
+                type="submit"
+              >
+                Save
+              </Button>
+            </div>
+          </form>
+        </Form>
+      )}
+    </div>
+  )
+}
