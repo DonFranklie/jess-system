ca6739d14ba3a9459761470d3f9146c62ca2e703
DonFranklie <donfranklie@gmail.com>
2024-03-25 08:06:40 +0300

    complete videos page

--- app/(dashboard)/(routes)/learning-videos/page.tsx
+++ app/(dashboard)/(routes)/learning-videos/page.tsx
@@ -1,8 +1,39 @@
-const LearningVideosPage = () => {
+import { getVideos } from "@/actions/get-videos";
+import { VideoList } from "@/components/video-list";
+import { VideoSearchInput } from "@/components/video-search-input";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+interface VideoPageProps {
+  searchParams: {
+    title: string;
+  }
+}
+
+const LearningVideosPage = async ({
+  searchParams
+}: VideoPageProps) => {
+  const { userId } = auth();
+  if(!userId) {
+    return redirect("/")
+  }
+
+
+  const videos = await getVideos({
+    userId,
+    ...searchParams
+  })
+
   return (
-    <div>
-      This is a LearningVideosPage
+   <>
+   <div className="px-6 pt-6 md:hidden md:mb-0 block">
+      <VideoSearchInput/>
     </div>
+   
+    <div className="p-6">
+        <VideoList items={videos}/>
+    </div>
+   </>
   );
 
 }
