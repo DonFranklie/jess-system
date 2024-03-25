353d04961f92c8e973c0cc033256086ea388c33d
DonFranklie <donfranklie@gmail.com>
2024-03-25 08:08:07 +0300

    video actions

---
+++ app/(dashboard)/(routes)/teacher/films/[filmId]/_components/actions.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import axios from "axios";
+import { Trash } from "lucide-react";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { useConfettiStore } from "@/hooks/use-confetti-store";
+
+interface ActionsProps {
+  disabled: boolean;
+  filmId: string;
+  isPublished: boolean;
+};
+
+export const Actions = ({
+  disabled,
+  filmId,
+  isPublished
+}: ActionsProps) => {
+  const router = useRouter();
+  const confetti = useConfettiStore();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(`/api/films/${filmId}/unpublish`);
+        toast.success("Video unpublished");
+      } else {
+        await axios.patch(`/api/films/${filmId}/publish`);
+        toast.success("Video published");
+        confetti.onOpen();
+      }
+
+      router.refresh();
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/films/${filmId}`);
+
+      toast.success("Video deleted");
+      router.refresh();
+      router.push(`/teacher/films`);
+    } catch {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <div className="flex items-center gap-x-2">
+      <Button
+        onClick={onClick}
+        disabled={disabled || isLoading}
+        variant="outline"
+        size="sm"
+      >
+        {isPublished ? "Unpublish" : "Publish"}
+      </Button>
+      <ConfirmModal onConfirm={onDelete}>
+        <Button size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
+    </div>
+  )
+}
