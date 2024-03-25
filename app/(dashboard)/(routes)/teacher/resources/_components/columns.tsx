1b5f3884bf81acacf1a081f050be71ffaac05ecc
DonFranklie <donfranklie@gmail.com>
2024-03-25 08:09:55 +0300

    Resurce Title data table

--- app/(dashboard)/(routes)/teacher/resources/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/resources/_components/columns.tsx
@@ -25,7 +25,7 @@ export const columns: ColumnDef<Resource>[] = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
         >
-          Title
+          Resource Title
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       )
